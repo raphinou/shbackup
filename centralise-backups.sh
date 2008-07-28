05d181e48e8a30aff18a485d61520455d5441f78
root <root@modb-base.(none)>
2008-07-28 21:49:07 +0200

    centralise backups now user the config file

--- centralise-backups.sh
+++ centralise-backups.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+. ./backup_vars.sh
+
+
 vservers=$(/usr/sbin/vserver-stat | /usr/bin/awk '$1!="0" && NR>1  { print $8 }')
 
 
 
 for vserver in $vservers; do
         backup_file=$(find /etc/vservers/${vserver}/vdir/data/backup -maxdepth 1 -type f)
-        [[ -n $backup_file ]] &&  cp $backup_file /data/backup/
+        [[ -n $backup_file ]] &&  cp $backup_file $backup_base
 done
