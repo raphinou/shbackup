83eb8358be904a112dfcd5aa2d62a4a91e32d1a5
Raphaël Bauduin <rb@raphinou.com>
2011-04-13 14:06:19 +0200

    added exclusion of files in archiving

--- archivefiles.sh
+++ archivefiles.sh
@@ -3,5 +3,5 @@
 . ./backup_vars.sh
 directories_to_archive="/etc /var/www /usr/local/bin /home/planet"
 
-/bin/tar jcvf $backup_dir/files.tar.bz2 $directories_to_archive
+/bin/tar -X archive_excluded.txt -jcvf $backup_dir/files.tar.bz2 $directories_to_archive
 
