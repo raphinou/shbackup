e12b90fc97c56c6b744d9e35a3bbcb4a6063aee2
Raphaël Bauduin <raph@bigbentobox.com>
2011-08-03 12:02:38 +0200

    centralise handles db dumps separately

--- centralise-backups.sh
+++ centralise-backups.sh
@@ -10,4 +10,13 @@ vservers=$(/usr/sbin/vserver-stat | /usr/bin/awk '$1!="0" && NR>1  { print $8 }'
 for vserver in $vservers; do
         backup_file=$(find /etc/vservers/${vserver}/vdir/data/backup -maxdepth 1 -type f)
         [[ -n $backup_file ]] &&  cp $backup_file $backup_base
+
+        # save the mysql dump seperately, easier to catch errors
+        database_dump=$(find /etc/vservers/${vserver}/vdir/data/backup -maxdepth 3 -type f -name mysql_databases.sql)
+        [[ -n $database_dump ]] &&  gzip -c $database_dump > $backup_base/${vserver}_mysql_databases_${date}.sql.gz
+
+        # save the postgresql dump seperately, easier to catch errors
+        database_dump=$(find /etc/vservers/${vserver}/vdir/data/backup -maxdepth 3 -type f -name postgresql_databases.sql)
+        [[ -n $database_dump ]] &&  gzip -c $database_dump > $backup_base/${vserver}_mysql_databases_${date}.sql.gz
+
 done
